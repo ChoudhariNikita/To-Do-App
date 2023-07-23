c0b515782ef37e1410aae666e82b05acd0dabcaf
ChoudhariNikita <choudharinikita96@gmail.com>
2023-07-23 19:12:45 +0530

    My current code that lacks 2 functions: check and delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,19 +5,34 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'To-Do-App';
   itemList = [' '];
   itemName = '';
   count = 0;
   see = true;
+  chkstat= false;
+  
   addItem() {
     if (this.count == 0) {
       this.itemList.pop();
     }
-    this.itemList.push(this.itemName);
-    this.see = false;
-    this.itemName = "";
-    this.count = 1;
+   
+    if(this.itemName!=""){
+      this.itemList.push(this.itemName);
+      this.see = false;
+      this.itemName = "";
+      this.count = 1;
+    }
+   
   }
+
+  deleteItem(){
+    if(this.chkstat==true){
+      this.see=true;
+    }
+  }
+
 }
+
