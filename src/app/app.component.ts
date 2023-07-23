fbcb1c6a9c7be44406a6a9f5c2ca336d8b56f26a
ChoudhariNikita <choudharinikita96@gmail.com>
2023-07-23 19:54:14 +0530

    Adding styles to components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,36 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent {
   title = 'To-Do-App';
   itemList = [' '];
+  // itemList: { name: string; completed: boolean }[] = [];
   itemName = '';
   count = 0;
   see = true;
-  chkstat= false;
-  
+  chkstat = false;
+  decor = 'none';
+
   addItem() {
     if (this.count == 0) {
       this.itemList.pop();
     }
-   
-    if(this.itemName!=""){
+
+    if (this.itemName != '') {
       this.itemList.push(this.itemName);
       this.see = false;
-      this.itemName = "";
+      this.itemName = '';
       this.count = 1;
     }
-   
   }
 
-  deleteItem(){
-    if(this.chkstat==true){
-      this.see=true;
+  deleteItem() {
+    if (this.chkstat === true) {
+      this.see = true;
     }
   }
 
+  getDecor() {
+    this.decor = 'line-through';
+  }
 }
-
